e4aba21a7859892a1241fc5a9c1fc76f4a7877eb
GiuliaGGG <144107205+GiuliaGGG@users.noreply.github.com>
2025-12-11 20:59:39 +0100

    directory changes

--- data/wayback.py
+++
@@ -1,6 +0,0 @@
-from waybackpy import WaybackMachineCDXServerAPI
-url = "https://google.com"
-user_agent = "my new app's user agent"
-cdx_api = WaybackMachineCDXServerAPI(url, user_agent)
-oldest_archive = cdx_api.oldest()
-print(oldest_archive.archive_url)
