e32487382be90e8fb7821c69048a260f279c051a
GiuliaGGG <144107205+GiuliaGGG@users.noreply.github.com>
2025-12-19 19:47:55 +0100

    gsynth base to test other counterfactuals

--- r/packages_scm/gsynth.R
+++ r/packages_scm/gsynth.R
@@ -5,6 +5,16 @@ panelview(revenue ~ boycotted, data = data,  index = c("ticker","time"), pre.pos
 system.time(
   out <- gsynth(
   revenue ~ boycotted,
+  + cost 
+  + depr_amort
+  + eps_basic 
+  + eps_diluted 
+  + interest_exp 
+  + operating_exp 
+  + r_and_d 
+  + shares_basic
+  + shares_diluted
+  + tax,
   data = data,
   index = c("ticker","time"),
   force = "two-way",
@@ -39,41 +49,6 @@ plot(out, type = "counterfactual", id = 'MCD',
 plot(out, type = "counterfactual", id = 'MCD', 
      raw = "all", legendOff = TRUE)
 
-
-# EM method 
-'The EM algorithm proposed by Gobillon and Magnac (2016) 
-takes advantage of the treatment group information in 
-the pre-treatment period. We implement this method. 
-The estimation takes more time, but the results are 
-very similar to that from the original method – the 
-coefficients will be slightly more precisely estimated.'
-system.time(
-  out <- gsynth(revenue ~ boycotted,
-                #+shares_basic+ assets,
-                data = data,  
-                index = c("ticker","time"),
-                EM = TRUE, 
-                force = "two-way",
-                inference = "parametric", 
-                se = TRUE, nboots = 500, r = c(0, 5), 
-                CV = TRUE, parallel = TRUE, cores = 4)
-)
-plot(out, main = "Estimated ATT (EM)")
-plot(out, theme.bw = FALSE) 
-plot(out, type = "gap", ylim = c(-1200000000,1200000000), xlab = "Period", 
-     main = "My GSynth Plot")
-plot(out, type = "raw")
-plot(out,type = "raw", legendOff = TRUE, ylim=c(-10,40), main="")
-plot(out, type = "counterfactual", raw = "none", main="")
-plot(out, type = "ct", raw = "none", main = "", 
-     shade.post = FALSE)
-plot(out, type = "counterfactual", raw = "band", 
-     xlab = "Time", ylim = c(0,30000000000))
-plot(out, type = "counterfactual", raw = "all")
-plot(out, type = "counterfactual", id = 'MCD')
-plot(out, type = "counterfactual", id = 'MCD', 
-     raw = "band", ylim = c(-10000000000, 30000000000))
-plot(out, type = "counterfactual", id = 'MCD', 
-     raw = "all", legendOff = TRUE)
-plot(out, type = "loadings")
-
+out$est.att
+out$est.avg
+out$est.beta
