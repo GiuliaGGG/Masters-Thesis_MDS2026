485509219b6063bbe49ad1552b5dc77499c02088
GiuliaGGG <144107205+GiuliaGGG@users.noreply.github.com>
2025-12-23 17:23:16 +0100

    prepare dataset

--- python/3__scm_prepare.py
+++ python/3__scm_prepare.py
@@ -29,21 +29,58 @@ def scm_prepare():
     df = drop_chronically_sparse_donors(df)
     df = pivot_wide(df)
 
+    df = complete_scm_time_grid(
+        df=df,
+        unit_col="ticker",
+        time_col="time"
+    )
+
+    df = impute_all_numeric_within_unit(
+        df=df,
+        unit_col="ticker",
+        time_col="time",
+        exclude_cols=["ticker"],  # treatment indicator (do NOT impute)
+        method="both"
+    )
+  
+    vars_to_standardize = [
+        "cost",
+        "depr_amort",
+        "eps_basic",
+        "eps_diluted",
+        "gross_profit",
+        "interest_exp",
+        "net_income",
+        "operating_exp",
+        "revenue",
+        "shares_basic",
+        "shares_diluted",
+        "tax"
+    ]
+
+    df = standardize_by_period0(
+        df=df,
+        unit_col="ticker",
+        time_col="time",
+        cols=vars_to_standardize,
+        method="ratio"   # recommended for revenue
+    )
+
+
 
     # -----------------
     # Save output
     # -----------------
     output_path = (
-        "./data/processed/data_check_cant_pivot.csv"
+        "./data/processed/data_standardized.csv"
     )
 
     df.to_csv(output_path, index=False)
+
+
 
     return df
 
 
 if __name__ == "__scm_prepare__":
     scm_prepare()
-
-
-
