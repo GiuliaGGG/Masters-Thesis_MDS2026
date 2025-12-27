2f40a23ba704ec57e504425656ece547cbcdf7f6
GiuliaGGG <144107205+GiuliaGGG@users.noreply.github.com>
2025-12-27 19:53:46 +0100

    adapt synth to my case

--- r/packages_scm/example_synth.R
+++ r/packages_scm/example_synth.R
@@ -2,42 +2,28 @@
 ### chunk number 1: 
 ###################################################
  library("Synth")
- data("basque")
- 
-	
-###################################################
-### chunk number 2: 
-###################################################
- basque[85:89, 1:4]
-	
+
+data <-read_csv('/Users/giuliamariapetrilli/Documents/GitHub/masters_thesis/data/processed/batch_2/data_batch_2_recent.csv')
+data_c <- data %>% 
+  mutate(company_id = as.integer(as.factor(ticker))) %>% as.data.frame()
+
 ###################################################
 ### chunk number 3: 
 ###################################################
- dataprep.out <-
-              dataprep(foo = basque,
-                       predictors = c("school.illit" , "school.prim" , "school.med" ,
-                                      "school.high" , "school.post.high" , "invest") ,
-                       predictors.op = "mean" ,
-                       time.predictors.prior = 1964:1969 ,
-                       special.predictors = list(
-                         list("gdpcap" , 1960:1969 , "mean"),
-                         list("sec.agriculture" ,      seq(1961,1969,2), "mean"),
-                         list("sec.energy" ,           seq(1961,1969,2), "mean"),
-                         list("sec.industry" ,         seq(1961,1969,2), "mean"),
-                         list("sec.construction" ,     seq(1961,1969,2), "mean"),
-                         list("sec.services.venta" ,   seq(1961,1969,2), "mean"),
-                         list("sec.services.nonventa" ,seq(1961,1969,2), "mean"),
-                         list("popdens", 1969, "mean")
-                                                ),
-                       dependent = "gdpcap",
-                       unit.variable = "regionno",
-                       unit.names.variable = "regionname",
-                       time.variable = "year",
-                       treatment.identifier = 17,
-                       controls.identifier = c(2:16,18),
-                       time.optimize.ssr = 1960:1969,
-                       time.plot = 1955:1997
-                       )
+dataprep.out <- dataprep(
+  foo = data_c,
+  dependent = "revenue_std",
+  predictors = "revenue_std",
+  predictors.op = "mean",
+  time.predictors.prior = 8045:8096,
+  unit.variable = "company_id",
+  time.variable = "time",
+  treatment.identifier = 7,
+  controls.identifier = c(1:6, 8:12),
+  time.optimize.ssr = 8045:8096,
+  time.plot = 8045:8102
+)
+
 	
 ###################################################
 ### chunk number 4: 
@@ -48,30 +34,6 @@
 ### chunk number 5: 
 ###################################################
  dataprep.out$Z1 #treated unit’s outcome values (Basque Country).
-	
-###################################################
-### chunk number 6: 
-###################################################
- dataprep.out$X1["school.high",] <-
-   dataprep.out$X1["school.high",] + dataprep.out$X1["school.post.high",]
- dataprep.out$X1 <-
-   as.matrix(dataprep.out$X1[-which(rownames(dataprep.out$X1)=="school.post.high"),])
- dataprep.out$X0["school.high",] <-
-   dataprep.out$X0["school.high",] + dataprep.out$X0["school.post.high",]
- dataprep.out$X0 <-
-   dataprep.out$X0[-which(rownames(dataprep.out$X0)=="school.post.high"),]
-
- lowest  <- which(rownames(dataprep.out$X0)=="school.illit")
- highest <- which(rownames(dataprep.out$X0)=="school.high")
-
- dataprep.out$X1[lowest:highest,] <-
-  (100*dataprep.out$X1[lowest:highest,]) /
-   sum(dataprep.out$X1[lowest:highest,])
- dataprep.out$X0[lowest:highest,] <-
-   100*scale(dataprep.out$X0[lowest:highest,],
-             center=FALSE,
-             scale=colSums(dataprep.out$X0[lowest:highest,])
-  )
 	
 ###################################################
 ### chunk number 7: 
@@ -114,10 +76,10 @@
 ###################################################
  path.plot(synth.res = synth.out,
            dataprep.res = dataprep.out,
-           Ylab = "real per-capita GDP (1986 USD, thousand)",
-           Xlab = "year",
-           Ylim = c(0,12),
-           Legend = c("Basque country","synthetic Basque country"),
+           Ylab = "Revenue (standardized from period 0)",
+           Xlab = "time",
+           Ylim = c(0.5,1.5),
+           Legend = c("McDonald's","synthetic McDonald's"),
            Legend.position = "bottomright"
            )
 	
@@ -126,8 +88,8 @@
 ###################################################
  gaps.plot(synth.res = synth.out,
            dataprep.res = dataprep.out,
-           Ylab = "gap in real per-capita GDP (1986 USD, thousand)",
-           Xlab = "year",
+           Ylab = "Revenue (standardized from period 0)",
+           Xlab = "time",
            Ylim = c(-1.5,1.5),
            Main = NA
            )
