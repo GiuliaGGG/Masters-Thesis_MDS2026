b5fc41064e182ee3b9b3bd0c3427153dd0b11a0b
GiuliaGGG <144107205+GiuliaGGG@users.noreply.github.com>
2026-01-02 18:49:04 +0100

    deleter filter firms

--- r/setup.r
+++ r/setup.r
@@ -14,7 +14,6 @@ library(augsynth)
 
 # read data 
 data <-read_csv('data/processed/batch_3/data.csv')
-data <- data %>%  filter(!ticker == 'HD') %>% filter(!ticker == 'NFLX')
 data_company_id <- data %>% 
   mutate(company_id = as.integer(as.factor(ticker))) %>% as.data.frame()
 
