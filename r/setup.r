6a6eb0cd156af8438d155e2c1477f61524f233e1
GiuliaGGG <144107205+GiuliaGGG@users.noreply.github.com>
2026-01-12 21:13:45 +0100

    r setup

--- r/setup.r
+++ r/setup.r
@@ -11,11 +11,24 @@ library(tidysynth)
 library(panelView)
 library(gsynth)
 library(augsynth)
+library(fixest)
 
 # read data 
-data <-read_csv('data/processed/batch_3/data.csv')
-data_company_id <- data %>% 
+data_log_simple <-read_csv('data/processed/batch_3/data_log_simple.csv')
+data_log_simple_company_id <- data_log_simple %>% 
   mutate(company_id = as.integer(as.factor(ticker))) %>% as.data.frame()
+
+time_lookup <- data_company_id %>%
+  distinct(time, time_label) %>%
+  arrange(time)
+
+time_labels <- setNames(
+  time_lookup$time_label,
+  time_lookup$time
+)
+
+time_breaks <- time_lookup$time[seq(1, nrow(time_lookup), by = 4)]
+time_labels <- time_lookup$time_label[seq(1, nrow(time_lookup), by = 4)]
 
 
 
